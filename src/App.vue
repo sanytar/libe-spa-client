12c6a99cd290a11714365047550b935145cb9564
synt3tic <mel04nax@gmail.com>
2023-04-14 16:24:19 +0300

    la-select ready

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,38 @@
 <script setup lang="ts">
 import { ref } from 'vue';
-import LaCheckbox from './components/UI/LaCheckbox/LaCheckbox.vue';
-const checked = ref(true);
+import LaSelect from './components/UI/LaSelect/LaSelect.vue';
+
+const options = [
+  {
+    id: 1,
+    name: 'Option 1',
+  },
+  {
+    id: 2,
+    name: 'Option 2',
+  },
+  {
+    id: 3,
+    name: 'Option 3',
+  },
+  {
+    id: 4,
+    name: 'Option 4',
+  },
+];
+
+const selectedOption = ref(
+  {
+    id: 4,
+    name: 'Option 4',
+  }
+);
+
 </script>
 
 <template>
-  <div class="p-8">
-    <la-checkbox v-model="checked" />
+  <div class="p-8 bg-dark-grey h-[100vh]">
+    <la-select v-model="selectedOption" :options="options"></la-select>
   </div>
 </template>
 
---
+++ src/components/UI/LaSelect/LaSelect.vue
@@ -0,0 +1,121 @@
+<script setup lang="ts">
+import { ref, computed } from 'vue';
+import LaSvg from '../LaSvg/LaSvg.vue';
+
+interface Option {
+  id: number;
+  name: string;
+}
+interface Props {
+  options: Option[];
+  modelValue?: Option;
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  modelValue: undefined,
+});
+const emit = defineEmits(['update:modelValue']);
+
+const selectedOption = ref<Option | null>(null);
+const areOptionsOpened = ref(false);
+
+const selectedOptionClasses = computed(() => {
+  return {
+    'la-select__selected': true,
+    'la-select__selected_opened': areOptionsOpened.value,
+  };
+});
+const iconClass = computed(() => {
+  return {
+    'la-select__icon': true,
+    'la-select__icon_opened': areOptionsOpened.value,
+  };
+});
+const optionClasses = (option: Option) => {
+  return {
+    option: true,
+    option_selected: option.id === selectedOption.value?.id,
+  };
+};
+const placeholder = computed(() => {
+  let result;
+  if(selectedOption.value) {
+    result = selectedOption.value.name;
+  } else if(props.modelValue) {
+    result = props.modelValue.name;
+  } else {
+    result = 'Выберите';
+  }
+  return result;
+});
+
+const selectOption = (option: Option) => {
+  selectedOption.value = option;
+  areOptionsOpened.value = !areOptionsOpened.value;
+  emit('update:modelValue', option);
+};
+</script>
+
+<template>
+  <div class="la-select">
+    <div :class="selectedOptionClasses" @click="areOptionsOpened = !areOptionsOpened">
+      <p>{{ placeholder }}</p>
+      <la-svg :class="iconClass" name="chevron" />
+    </div>
+    <transition name="options">
+      <ul v-if="areOptionsOpened" class="la-select__options">
+        <li 
+          v-for="option in props.options" 
+          :key="option.id" 
+          :class="optionClasses(option)"
+          @click="selectOption(option)"
+        >
+          {{ option.name }}
+        </li>
+      </ul>
+    </transition>
+  </div>
+  
+</template>
+
+<style scoped>
+.la-select {
+  @apply relative;
+}
+.la-select__selected {
+  @apply flex justify-between z-10 p-4 rounded-full w-full bg-dark-red hover:cursor-pointer hover:shadow-md select-none  
+  text-regular-white duration-[120ms];
+}
+
+.la-select__selected_opened {
+  @apply rounded-b-none rounded-t-3.5xl border-b-0 hover:shadow-none duration-[120ms];
+}
+
+.la-select__icon {
+  @apply duration-300;
+}
+.la-select__icon_opened {
+  @apply origin-center -rotate-180 duration-300;
+}
+
+.la-select__options {
+  @apply absolute flex flex-col bg-dark-red w-full text-regular-white select-none rounded-b-3.5xl overflow-y-hidden;
+}
+
+.option {
+  @apply p-4 hover:cursor-pointer hover:bg-light-red/60;
+}
+
+.option_selected {
+  @apply bg-light-red/60;
+}
+
+.options-enter-active,
+.options-leave-active {
+  @apply transition-opacity duration-200;
+}
+.options-enter-from,
+.options-leave-to {
+  @apply opacity-0;
+}
+</style>
