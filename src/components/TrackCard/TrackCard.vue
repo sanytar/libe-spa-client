d6981be45252a93377f6052393ed1757f30b35e8
synt3tic <mel04nax@gmail.com>
2023-04-26 13:05:52 +0300

    track-card ready

--- src/components/TrackCard/TrackCard.vue
+++ src/components/TrackCard/TrackCard.vue
@@ -20,6 +20,13 @@ const cardClasses = computed(() => {
   };
 });
 
+const cardCoverClasses = computed(() => {
+  return {
+    'track-card__cover': true,
+    'track-card__cover_lg': props.size === 'lg',
+  };
+});
+
 const cardInfoClasses = computed(() => {
   return {
     'track-card__info': true,
@@ -32,25 +39,43 @@ const cardInfoClasses = computed(() => {
 
 <template>
   <div :class="cardClasses">
-    <div class="w-20 h-20 rounded-lg bg-regular-yellow"></div><!-- временная заплатка -->
+    <div :class="cardCoverClasses"></div><!-- временная заплатка -->
     <div :class="cardInfoClasses">
       <h4>{{ props.name }}</h4>
       <p>{{ props.author }}</p>
       <p v-if="size === 'lg'" class="info-timeline">1:39 / 3:28</p>
     </div>
-    <la-button id="button" icon="play" size="sm" variation="transparent" @click.stop/>
+    <la-button 
+      v-if="!(size === 'sm')" 
+      id="button" 
+      icon="play" 
+      size="sm" 
+      variation="transparent" 
+      @click.stop
+    />
   </div>
 </template>
 
 <style scoped>
 .track-card {
-  @apply flex items-center gap-3 p-3 w-fit rounded-xl bg-transparent border border-solid border-dark-red cursor-default
+  @apply flex items-center gap-3 p-3 w-fit rounded-xl bg-transparent border border-solid border-teal-200 cursor-default
   transition-all hover:bg-light-grey/10 hover:cursor-pointer active:scale-98;
 }
 
+.track-card_sm {
+  @apply gap-1.5 border-none;
+}
+
+.track-card__cover {
+  @apply w-11 h-11 rounded-lg bg-regular-yellow;
+}
+
+.track-card__cover_lg {
+  @apply w-20 h-20;
+}
+
 .track-card__info {
-  @apply flex flex-col items-start justify-between w-20 h-11 text-regular-white whitespace-nowrap
-  overflow-hidden text-ellipsis;
+  @apply flex flex-col items-start justify-between w-20 h-11 text-regular-white whitespace-nowrap;
 }
 
 .track-card__info_lg {
@@ -58,14 +83,10 @@ const cardInfoClasses = computed(() => {
 }
 
 .track-card__info h4 {
-  @apply font-medium leading-5;
+  @apply w-full font-medium leading-5 whitespace-nowrap text-ellipsis overflow-hidden;
 }
 
 .track-card__info p {
-  @apply text-sm leading-4;
+  @apply w-full text-sm leading-4 overflow-hidden text-ellipsis;
 }
-
-/* .info-timeline {
-
-} */
 </style>
